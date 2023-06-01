6046aad862c4ca6a756373b631107bd7754937a3
voyager003 <miraclex000@gmail.com>
2023-06-01 15:58:29 +0900

    spring init commit

---
+++ src/main/java/toy/shoppingmall/ToyShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package toy.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/shoppingmall/ToyShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
