fd6825de2497c7ce73f3b07ae62af232ed815a3f
voyager003 <miraclex000@gmail.com>
2023-06-05 13:46:56 +0900

    ✨Feat: 회원가입, 로그인 라우터

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -9,7 +9,7 @@
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">쇼핑몰 토이 프로젝트입니다.</h1>
           <p class="mt-6 text-lg leading-8 text-gray-600">회원가입 및 로그인을 해주세요.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+            <a href="/signup" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">회원가입</a>
           </div>
         </div>
       </div>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from "@/views/ProductsView.vue";
+import SignupPage from "@/components/SignupPage.vue";
+import LoginPage from "@/components/LoginPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/products',
       name: 'products',
       component: ProductsView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupPage
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
     }
   ]
 })
