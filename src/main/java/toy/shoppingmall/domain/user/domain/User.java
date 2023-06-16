a00b3cc84e4791ff7fd140923428b2e10cb3de7b
voyager003 <miraclex000@gmail.com>
2023-06-16 11:06:13 +0900

    🔨Fix(User): Builder 및 NoArgsConstructor, nullable 설정

--- src/main/java/toy/shoppingmall/domain/user/domain/User.java
+++ src/main/java/toy/shoppingmall/domain/user/domain/User.java
@@ -1,28 +1,31 @@
 package toy.shoppingmall.domain.user.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import toy.shoppingmall.domain.model.Address;
 import toy.shoppingmall.domain.model.Role;
 import toy.shoppingmall.domain.order.domain.Order;
+import toy.shoppingmall.domain.user.dto.UserSignupRequest;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user_info")
 public class User {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
     private Long id;
-    @Column(name = "user_email")
+    @Column(name = "user_email", nullable = false)
     private String email;
 
-    @Column(name = "user_password")
+    @Column(name = "user_password", nullable = false)
     private String Password;
 
     @Enumerated(EnumType.STRING)
@@ -33,4 +36,10 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Order> orders = new ArrayList<>();
 
+    @Builder
+    public User(String email, String password, Role role) {
+        this.email = email;
+        this.Password = password;
+        this.role = role;
+    }
 }
