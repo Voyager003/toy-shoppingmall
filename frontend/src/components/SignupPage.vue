6b3e505345f265a86ab3efc19a8dc2f0cd9058e2
voyager003 <miraclex000@gmail.com>
2023-07-04 23:13:43 +0900

    ✨Feat: 회원가입 기능 axios 추가

--- frontend/src/components/SignupPage.vue
+++ frontend/src/components/SignupPage.vue
@@ -5,33 +5,41 @@
         <img class="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="">
       </div>
 
-      <form class="mt-6">
+      <form class="mt-6" @submit.prevent="signup">
         <div>
-          <label for="email" class="block text-sm text-gray-800 dark:text-gray-200">Email</label>
-          <input type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+          <label for="email" class="block text-sm text-gray-800 dark:text-gray-200">이메일</label>
+          <input v-model="email" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
         </div>
         <div class="mt-4">
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">Password</label>
+            <label for="password" class="block text-sm text-gray-800 dark:text-gray-200">비밀번호</label>
           </div>
-          <input type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+          <input v-model="password" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+        </div>
+
+        <div class="mt-4 flex">
+          <div class="flex items-center mr-28">
+            <input id="seller-checkbox" v-model="role" type="radio" value="ROLE_SELLER" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <label for="seller-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">판매자</label>
+          </div>
+          <div class="flex items-center">
+            <input id="customer-checkbox" v-model="role" type="radio" value="ROLE_CUSTOMER" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <label for="customer-checkbox" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">구매자</label>
+          </div>
         </div>
         <div class="mt-6">
-          <button class="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-            Sign up
+          <button type="submit" class="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+            회원가입
           </button>
         </div>
       </form>
-
       <div class="flex items-center justify-between mt-4">
         <span class="w-1/5 border-b dark:border-gray-600 lg:w-1/5"></span>
-
         <a class="text-xs text-center text-gray-500 uppercase dark:text-gray-400">
           or login with Social Media
         </a>
         <span class="w-1/5 border-b dark:border-gray-400 lg:w-1/5"></span>
       </div>
-
       <div class="flex items-center mt-6 -mx-2">
         <button type="button" class="flex items-center justify-center w-full px-6 py-2 mx-2 text-sm font-medium text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:bg-blue-400 focus:outline-none">
           <svg class="w-4 h-4 mx-2 fill-current" viewBox="0 0 24 24">
@@ -46,8 +54,38 @@
 </template>
 
 <script>
+import axios from "axios";
+import router from "@/router";
+
 export default {
-  name: "SignupPage"
+  name: "SignupPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: ""
+    };
+  },
+  methods: {
+    signup() {
+      const user = {
+        email: this.email,
+        password: this.password,
+        role: this.role
+      };
+      axios.post("/signup", JSON.stringify(user), {
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(response => {
+          router.replace("/login");
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
 
