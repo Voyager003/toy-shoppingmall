d901ba6d349a4ce3337ba13ad2eca015b7c3b0fb
voyager003 <miraclex000@gmail.com>
2023-07-11 23:54:48 +0900

    ✨Feat: Security UserDetails 정보 클래스 추가

---
+++ src/main/java/toy/shoppingmall/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package toy.shoppingmall.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import toy.shoppingmall.domain.user.dao.UserRepository;
+import toy.shoppingmall.domain.user.domain.User;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Email Not Found with email: " + email));
+        return UserDetailsImpl.build(user);
+    }
+}
