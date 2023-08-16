f4537aac86d950414faf805e217f8808b5fa42df
voyager003 <miraclex000@gmail.com>
2023-08-16 23:48:43 +0900

    ✨Feat: 누락된 @SuperBuilder, @NoArgsConstructor 추가

--- src/main/java/toy/shoppingmall/domain/item/domain/Movie.java
+++ src/main/java/toy/shoppingmall/domain/item/domain/Movie.java
@@ -3,8 +3,12 @@ package toy.shoppingmall.domain.item.domain;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@SuperBuilder
+@NoArgsConstructor
 @DiscriminatorValue("M")
 @Getter
 public class Movie extends Item {
