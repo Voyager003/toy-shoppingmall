45ca33393bb0597dbe34be3d09b37c192a93de44
voyager003 <miraclex000@gmail.com>
2023-09-19 23:12:41 +0900

    Feat: 상품 Pageable 조회 테스트 코드 작성

--- src/test/java/toy/shoppingmall/domain/item/api/ItemApiTest.java
+++ src/test/java/toy/shoppingmall/domain/item/api/ItemApiTest.java
@@ -6,25 +6,38 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import toy.shoppingmall.domain.item.application.ItemService;
+import toy.shoppingmall.domain.item.domain.Item;
 import toy.shoppingmall.domain.item.dto.ItemRequest;
 
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(controllers = ItemApi.class)
 @WithMockUser
 class ItemApiTest {
 
-    @Autowired MockMvc mockMvc;
-    @Autowired ObjectMapper objectMapper;
-    @MockBean ItemService itemService;
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @MockBean
+    ItemService itemService;
 
     @Test
     @DisplayName("상품 등록 성공 케이스")
@@ -105,6 +118,25 @@ class ItemApiTest {
                         .file(ItemRequest)
                         .with(csrf()))
                 .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("페이징 조회 동작 테스트")
+    void getItems_PageableTest() throws Exception {
+        /**
+         * given : Pageable이 주어진다.
+         * when : /products?page=1&sort=id.desc GET 요청을 했을 때
+         * then : STATUS 200을 반환한다.
+         */
+
+        Page<Item> pageableObj = new PageImpl<>(Collections.emptyList());
+        when(itemService.findItems(any(Pageable.class))).thenReturn(pageableObj);
+
+        mockMvc.perform(get("/products?page=1&sort=id.desc")
+                        .contentType(APPLICATION_JSON)
+                        .with(csrf()))
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 }
--- src/test/java/toy/shoppingmall/domain/item/application/ItemServiceTest.java
+++ src/test/java/toy/shoppingmall/domain/item/application/ItemServiceTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 import toy.shoppingmall.domain.item.dao.ItemRepository;
 import toy.shoppingmall.domain.item.domain.Book;
+import toy.shoppingmall.domain.item.domain.Item;
 import toy.shoppingmall.domain.item.dto.ItemRequest;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 
 @SpringBootTest
@@ -54,5 +61,47 @@ class ItemServiceTest {
         assertThat(savedBook.getPrice()).isEqualTo(20000);
         assertThat(savedBook.getStockQuantity()).isEqualTo(5);
         assertThat(savedBook.getAuthor()).isEqualTo("Author Name");
+    }
+
+    @Test
+    @DisplayName("상품 페이징 테스트")
+    void findItems_PagingTest() throws IOException {
+        /**
+         * given : 10개의 상품을 등록한다.
+         * when : 페이징 정보를 3으로 설정하고 상품 목록을 조회할 때
+         * then : 예상한 페이지 당 항목 수, 전체 항목 수, 다음 페이지 존재 여부를 확인한다.
+         */
+
+        IntStream.range(0, 10).forEach(i -> {
+            ItemRequest itemRequest = ItemRequest.builder()
+                    .name("Book Item " + i)
+                    .price(20000 + i)
+                    .stockQuantity(5)
+                    .category("book")
+                    .categoryDetail("Author Name " + i)
+                    .build();
+
+            MockMultipartFile multipartFile = new MockMultipartFile(
+                    "file",
+                    "test" + i + ".txt",
+                    "text/plain",
+                    ("Test content " + i).getBytes()
+            );
+
+            try {
+                itemService.registerItem(itemRequest, multipartFile);
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        });
+
+        Pageable pageable = PageRequest.of(0, 3, DESC, "id");
+
+        Page<Item> pageableItems = itemService.findItems(pageable);
+
+        assertEquals(3, pageableItems.getContent().size());
+        assertEquals(10, pageableItems.getTotalElements());
+        assertEquals(4, pageableItems.getTotalPages());
+        assertTrue(pageableItems.hasNext());
     }
 }
